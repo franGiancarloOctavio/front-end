1893ea099c3e9cee529cb5047e16731433e71048
fran <fmls1989@gmail.com>
2018-12-16 11:42:44 +0100

    wip

--- myApp/src/app/app.component.ts
+++ myApp/src/app/app.component.ts
@@ -1,51 +1,35 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { TranslateService } from '@ngx-translate/core';
 import { Config, Nav, Platform } from 'ionic-angular';
 
-import { FirstRunPage } from '../pages';
-import { Settings } from '../providers';
-
 @Component({
-  template: `<ion-menu [content]="content">
-    <ion-header>
-      <ion-toolbar>
-        <ion-title>Pages</ion-title>
-      </ion-toolbar>
-    </ion-header>
-
-    <ion-content>
-      <ion-list>
-        <button menuClose ion-item *ngFor="let p of pages" (click)="openPage(p)">
-          {{p.title}}
-        </button>
-      </ion-list>
-    </ion-content>
-
-  </ion-menu>
-  <ion-nav #content [root]="rootPage"></ion-nav>`
+  template: 'app.html'
 })
-export class MyApp {
-  rootPage = FirstRunPage;
+export class MyApp implements OnInit{
 
-  @ViewChild(Nav) nav: Nav;
+  @ViewChild(Nav) navCtrl: Nav;
 
-  pages: any[] = [
-    { title: 'Tutorial', component: 'TutorialPage' },
-    { title: 'Welcome', component: 'WelcomePage' },
-    { title: 'Tabs', component: 'TabsPage' },
-    { title: 'Cards', component: 'CardsPage' },
-    { title: 'Content', component: 'ContentPage' },
-    { title: 'Login', component: 'LoginPage' },
-    { title: 'Signup', component: 'SignupPage' },
-    { title: 'Master Detail', component: 'ListMasterPage' },
-    { title: 'Menu', component: 'MenuPage' },
-    { title: 'Settings', component: 'SettingsPage' },
-    { title: 'Search', component: 'SearchPage' }
-  ]
 
-  constructor(private translate: TranslateService, platform: Platform, settings: Settings, private config: Config, private statusBar: StatusBar, private splashScreen: SplashScreen) {
+  
+
+  // pages: any[] = [
+  //   { title: 'Tutorial', component: 'TutorialPage' },
+  //   { title: 'Welcome', component: 'WelcomePage' },
+  //   { title: 'Tabs', component: 'TabsPage' },
+  //   { title: 'Cards', component: 'CardsPage' },
+  //   { title: 'Content', component: 'ContentPage' },
+  //   { title: 'Login', component: 'LoginPage' },
+  //   { title: 'Signup', component: 'SignupPage' },
+  //   { title: 'Master Detail', component: 'ListMasterPage' },
+  //   { title: 'Menu', component: 'MenuPage' },
+  //   { title: 'Settings', component: 'SettingsPage' },
+  //   { title: 'Search', component: 'SearchPage' }
+  // ]
+
+
+  constructor(private translate: TranslateService, platform: Platform, private config: Config, private statusBar: StatusBar, private splashScreen: SplashScreen) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -55,6 +39,13 @@ export class MyApp {
     this.initTranslate();
   }
 
+  ngOnInit(){
+    console.log(this.navCtrl);
+    
+    this.navCtrl.setRoot('LoginPage');
+  }
+
+  
   initTranslate() {
     // Set the default language for translation strings, and the current language.
     this.translate.setDefaultLang('en');
@@ -81,9 +72,6 @@ export class MyApp {
     });
   }
 
-  openPage(page) {
-    // Reset the content nav to have just this page
-    // we wouldn't want the back button to show in this scenario
-    this.nav.setRoot(page.component);
-  }
+
 }
+
